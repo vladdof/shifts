030f4631084e47c81750a65338a53075685b83db
Vladislav Efanov <vladislav.efanov@gmail.com>
2021-05-12 11:38:39 +0200

    Создал компонент Header и Menu.

---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Menu from './Menu';
+
+const HeaderElement = styled.header`
+    background: #e8eaf0;
+
+    @media (min-width: 1024px) {
+        padding: 45px 0 0;
+        min-height: 100vh;
+        box-shadow: 0 0 16px rgba(0,0,0,.08);
+        display: flex;
+        flex-direction: column;
+        width: var(--nav-bar-width);
+        overflow: visible;
+    }
+`;
+
+const links = [
+    'My Dashboard',
+    'People',
+    'Operations',
+    'Schedule Planning',
+    'Administration',
+    'Action Tracking'
+];
+
+
+const Header = () => (
+    <HeaderElement>
+        <div className="" as="button" onClick={() => alert('It works!')}>
+            <span className="icon">
+            </span>
+        </div>
+
+        <nav>
+            <button className="menu__button" type="button" aria-expanded="false" aria-controls="nemu">
+                <svg className="menu__icon" aria-hidden="true" width="32" height="32">
+                    <use className="menu__icon-menu" href="images/sprite.svg#menu"></use>
+                    <use className="menu__icon-close" href="images/sprite.svg#close"></use>
+                </svg>
+            </button>
+            <Menu links={links} />
+        </nav>
+    </HeaderElement>
+);
+
+export default Header;
