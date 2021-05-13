a7ac79a34c0ec7bb818658c94b3a9c410e03de2b
Vladislav Efanov <vladislav.efanov@gmail.com>
2021-05-13 23:09:32 +0200

    Добавил сервисный компонент с данными таблицы.

---
+++ src/service/FetchData.js
@@ -0,0 +1,41 @@
+const FetchData = () => {
+    const getHeadersTable = () => ['Date', 'Shift type', 'Period', 'Hours', 'Salary (gross)', 'Bonus (gross)'];
+    const getRowsTable = () => [
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+        ['January 06, 2021', 'Afternoon', '15:00 - 23:00', '8h', '$1,630', '30'],
+    ];
+
+    return { getRowsTable, getHeadersTable }
+};
+
+export default FetchData;
