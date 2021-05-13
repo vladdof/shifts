fd62fe89326f3079b4ccc57e6fba7935c46d6d33
Vladislav Efanov <vladislav.efanov@gmail.com>
2021-05-13 20:59:53 +0200

    Создал Мультиселект и настроил его.

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,23 +1,55 @@
 import React, { useState } from 'react';
 import MultiSelect from 'react-multi-select-component';
+import styled, { createGlobalStyle } from 'styled-components';
 
-export default function Select() {
+const GlobalStyle = createGlobalStyle`
+    .multi-select {
+        max-width: 165px;
+        width: 100%;
+    }
+
+    .rmsc {
+        --rmsc-border: #dedfe2;
+        --rmsc-radius: 6px;
+        --rmsc-h: 44px;
+    }
+`;
+
+const Select = () => {
+    const getDate = () => {
+        return new Date().getFullYear();
+    };
+
     const options = [
-        { label: "Grapes 🍇", value: "grapes" },
-        { label: "Mango 🥭", value: "mango" },
-        { label: "Strawberry 🍓", value: "strawberry" }
+        { label: `January, ${getDate()}`, value: `january` },
+        { label: `February, ${getDate()}`, value: `february` },
+        { label: `March, ${getDate()}`, value: `march` },
+        { label: `April, ${getDate()}`, value: `april` },
+        { label: `May, ${getDate()}`, value: `may` },
+        { label: `June, ${getDate()}`, value: `june` },
+        { label: `July, ${getDate()}`, value: `july` },
+        { label: `August, ${getDate()}`, value: `august` },
+        { label: `September, ${getDate()}`, value: `september` },
+        { label: `October, ${getDate()}`, value: `october` },
+        { label: `November, ${getDate()}`, value: `november` },
+        { label: `December, ${getDate()}`, value: `december` }
     ];
 
     const [selected, setSelected] = useState([]);
 
     return (
-        <div>
-            <MultiSelect
-                options={options}
-                value={selected}
-                onChange={setSelected}
-                labelledBy={"Select"}
-            />
-        </div>
+        <>
+        <GlobalStyle />
+        <MultiSelect
+            options={options}
+            value={selected}
+            onChange={setSelected}
+            labelledBy={'Select'}
+            selectAllLabel={'All dates'}
+            disableSearch
+        />
+        </>
     );
 }
+
+export default Select;
